fbbf0b1be6d6c0fc5b2a08c90dfd17cfd4732d16
educharrone@hotmail.com <educharrone@hotmail.com>
2021-11-03 20:14:38 -0300

    Backend project created

---
+++ backend/src/main/resources/data.sql
@@ -0,0 +1,14 @@
+INSERT INTO tb_user (name, email, password) values('Alex', 'alex@gmail.com', '112');
+INSERT INTO tb_user (name, email, password) values('Maria', 'maria@gmail.com', '332');
+INSERT INTO tb_user (name, email, password) values('Edu', 'edu@gmail.com', '555');
+
+Insert into tb_role(authority) values('ROLE_STUDENT');
+Insert into tb_role(authority) values('ROLE_INSTRUCTOR');
+Insert into tb_role(authority) values('ROLE_ADMIN');
+
+INSERT INTO tb_user_role(user_id,role_id) values(1,1);
+INSERT INTO tb_user_role(user_id,role_id) values(2,1);
+INSERT INTO tb_user_role(user_id,role_id) values(2,2);
+INSERT INTO tb_user_role(user_id,role_id) values(3,1);
+INSERT INTO tb_user_role(user_id,role_id) values(3,2);
+INSERT INTO tb_user_role(user_id,role_id) values(3,3);
