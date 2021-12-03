187da6bf9af68b44161c26836e90f4847d367c65
educharrone@hotmail.com <educharrone@hotmail.com>
2021-12-03 19:38:13 -0300

    Course

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -13,3 +13,5 @@ INSERT INTO tb_user_role(user_id,role_id) values(2,2);
 INSERT INTO tb_user_role(user_id,role_id) values(3,1);
 INSERT INTO tb_user_role(user_id,role_id) values(3,2);
 INSERT INTO tb_user_role(user_id,role_id) values(3,3);
+
+INSERT INTO tb_Course(name, img_Uri, img_Gray_Uri) values ('Curso HTML', 'https://c.pxhere.com/images/07/b2/f8ed07d6f1a9c740e7703cbb99ee-1443423.jpg!d','https://static.thenounproject.com/png/1574560-200.png'); 
