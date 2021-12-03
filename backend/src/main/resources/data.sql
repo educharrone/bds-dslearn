e6199b86603ba2f5b7ba6bf3d4c1d750a7d31253
educharrone@hotmail.com <educharrone@hotmail.com>
2021-12-03 18:28:04 -0300

    config file

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -1,10 +1,11 @@
 INSERT INTO tb_user (name, email, password) values('Alex', 'alex@gmail.com', '112');
 INSERT INTO tb_user (name, email, password) values('Maria', 'maria@gmail.com', '332');
-INSERT INTO tb_user (name, email, password) values('Edu', 'edu@gmail.com', '555');
+INSERT INTO tb_user (name, email, password) values('Edu', 'edu@gmail.com', '333');
 
 Insert into tb_role(authority) values('ROLE_STUDENT');
 Insert into tb_role(authority) values('ROLE_INSTRUCTOR');
 Insert into tb_role(authority) values('ROLE_ADMIN');
+
 
 INSERT INTO tb_user_role(user_id,role_id) values(1,1);
 INSERT INTO tb_user_role(user_id,role_id) values(2,1);
